876f84e136e99325f39f7b39e6b83cf2b855a2d9
Chakrit Wichian <chakrit@oozou.com>
2013-04-08 00:04:28 +0700

    Adds a few more generation mode for speed/readability.

--- index.js
+++ index.js
@@ -6,11 +6,73 @@ exports = module.exports = (function() {
     , inherits = require('util').inherits
     , Readable = require('readable-stream');
 
+  var OPTS =
+    { mode: 'random'
+    , defaultSize: 4096
+    };
+
+  // generator modes
+  var generators =
+    { '0':
+      function(size, cb) {
+        var buf = new Buffer(size);
+        buf.fill(0);
+        cb.call(this, null, buf);
+      }
+
+    , '01':
+      function(size, cb) {
+        var buf = new Buffer(size), i;
+
+        for (i = 0; i < size; i++)
+          buf[i] = [0, 1][i % 2];
+
+        cb.call(this, null, buf);
+      }
+
+    , random:
+      function(size, cb) {
+        var me = this;
+        randomBytes(size, function(e, buf) { cb.call(me, e, buf); });
+      }
+
+    , alpha:
+      function(size, cb) {
+        var me = this;
+        generators.random(size, function(e, buf) {
+          var i;
+          for (i = 0; i < size; i++)
+            buf[i] = 97 + buf[i] % 26; // 97 is ascii 'a'
+
+          cb.call(me, e, buf);
+        });
+      }
+    };
+
+  // core firehose
+  function RandStream(options) {
+    var mode, defaultSize, generator;
+
+    Readable.call(this, options);
+    if (options) {
+      mode = options.mode || OPTS.mode;
+      defaultSize = options.defaultSize || OPTS.defaultSize;
+    }
+
+    this._generator = generators[mode];
+    this._read = function(size) {
+      var me = this;
+      size = size || defaultSize;
+
+      this._generator(size, function(e, buffer) {
+        if (me.push(buffer)) process.nextTick(function() {
+          return me._read(size);
+        });
+      });
+    };
+  };
+
   inherits(RandStream, Readable);
-
-  function RandStream() { Readable.call(this); };
-  RandStream.prototype._read = randomBytes;
-
   return RandStream;
 
 })();
@@ -18,14 +80,14 @@ exports = module.exports = (function() {
 
 if (require && require.main === module) (function(RandStream) {
 
-  var firehose = new RandStream();
-  firehose.on('data', function(chunk) {
-    return process.stdout.write(chunk.toString('ascii'));
-  });
+  var mode = process.argv[2] || 'random'
+    , firehose = new RandStream({ mode: mode });
 
-  process.on('SIGINT', function() {
-    firehose.pause();
-    firehose.removeAllListeners('data');
+  firehose.pipe(process.stdout);
+
+  process.once('SIGINT', function() {
+    firehose.pause()
+    firehose.unpipe(process.stdout);
   });
 
 })(module.exports);
